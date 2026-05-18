dc565818b8de69c3889b0abf001c3bd41cb9f737
din1209-nguyen <nguyendin12092005@gmail.com>
2026-05-18 18:29:27 +0700

    feat(car): configure redis caching

--- tayota-backend/car-service/src/main/java/com/tayota/carservice/CarServiceApplication.java
+++ tayota-backend/car-service/src/main/java/com/tayota/carservice/CarServiceApplication.java
@@ -2,7 +2,9 @@ package com.tayota.carservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class CarServiceApplication {
 
